08b55cc9b5c264546878433275949b4008c01a4d
Crystal Sudler <178843998+sudler316@users.noreply.github.com>
2025-04-06 19:22:58 -0400

    Attempt #1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,9 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    if (times < 0) return "ERROR";
+    let word = "";
+    for (let i = 0; i < times; i++) {
+        string += word;
+    }
 
 };
 
