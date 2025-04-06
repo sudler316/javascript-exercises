9ebeda751343ea9f9bdbaeccdf45715a8bcc7853
Crystal Sudler <178843998+sudler316@users.noreply.github.com>
2025-04-06 19:24:39 -0400

    Attempt #2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,10 @@
 const repeatString = function(word, times) {
     if (times < 0) return "ERROR";
-    let word = "";
+    let string = "";
     for (let i = 0; i < times; i++) {
         string += word;
     }
-
+return string
 };
 
 // Do not edit below this line
