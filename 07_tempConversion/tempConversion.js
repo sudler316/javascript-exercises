51b794d91354a5e0b01563fe956fdda5f0f7b646
Crystal Sudler <178843998+sudler316@users.noreply.github.com>
2025-04-11 21:09:24 -0400

    Assignment Complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  // Conversion equation F -> C
+  let celsius = ((fahrenheit - 32)*(5/9))
+  // Rounding to 1 decimal
+  return Math.round(celsius * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  // Conversion equation C -> F
+  let fahrenheit = ((celsius * (9/5)) + 32)
+  // Rounding to 1 decimal
+  return Math.round(fahrenheit * 10) / 10
 };
 
 // Do not edit below this line
