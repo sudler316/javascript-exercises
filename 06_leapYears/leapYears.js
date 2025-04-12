b5a3e9c8d9e094da6f78858aca8fec0d868a0d14
Crystal Sudler <178843998+sudler316@users.noreply.github.com>
2025-04-11 20:48:03 -0400

    Assignment Complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isDivisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isDivisibleByFourHund = year % 400 === 0;
 
+
+    if (
+        (isDivisibleByFour) && (!isCentury || isDivisibleByFourHund)
+    ) { 
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
